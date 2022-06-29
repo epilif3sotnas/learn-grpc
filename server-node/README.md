c2f25aba0f117710b8252f8fb337c07f26ca7bf0
Filipe Santos <filipesantos1999@hotmail.com>
2022-06-29 23:18:05 +0100

    Add server node

---
+++ server-node/README.md
@@ -0,0 +1,8 @@
+# Run
+
+Run the server
+
+```
+cd {PATH_REPOSITORY}/server-node
+npm start
+```
---
+++ server-node/src/Main.js
@@ -0,0 +1,34 @@
+var PROTO_PATH = __dirname + '/proto/Hinterface.proto';
+
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    }
+);
+
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).hello;
+
+function helloWorld (call, callback) {
+    console.log(call.request);
+    callback(null, {message: true}); // Fix this it is sending false
+}
+
+function main () {
+    var server = new grpc.Server();
+    server.addService(hello_proto.Hello.service, {helloWorld: helloWorld});
+    server.bindAsync('localhost:5052', grpc.ServerCredentials.createInsecure(), () => {
+        console.log("Server starting...");
+        server.start();
+        console.log("Server started!");
+    });
+}
+
+main()
